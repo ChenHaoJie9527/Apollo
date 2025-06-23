2cf5c388d8678283e886d571e370f29c40b774a2
momotom <a18520145493@163.com>
2025-06-23 22:46:59 +0800

    test: Serialize new Date via encodeURIComponent test

--- test/fallback-options.spec.ts
+++ test/fallback-options.spec.ts
@@ -8,6 +8,7 @@ describe("serializeParams", () => {
     ${{ key1: true, key2: false }} | ${"key1=true&key2=false"}
     ${{key1: '123', key2: 1, key3: undefined, key4: null}} | ${'key1=123&key2=1&key4=null'}
     ${{key1: '', key2: 0, key3: new Date("2024-06-09T12:34:56.789Z")}} | ${'key1=&key2=0&key3=%222024-06-09T12%3A34%3A56.789Z%22'}
+    ${{key1: '', key2: 0, key3: new Date()}} | ${`key1=&key2=0&key3=${encodeURIComponent(JSON.stringify(new Date()))}`}
   `("test case serializeParams %#", ({ params, output }) => {
     expect(fallbackOptions.serializeParams(params)).toBe(output);
   });
