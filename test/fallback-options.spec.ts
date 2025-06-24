bd491b876af018af5893e9eb6b5a5f783e263cca
momotom <a18520145493@163.com>
2025-06-24 22:29:57 +0800

    test: Key is an array type

--- test/fallback-options.spec.ts
+++ test/fallback-options.spec.ts
@@ -4,12 +4,13 @@ import { fallbackOptions } from "../src/utils/fallback-options";
 
 describe("serializeParams", () => {
   test.each`
-    params                         | output
-    ${{ key1: true, key2: false }} | ${"key1=true&key2=false"}
-    ${{key1: '123', key2: 1, key3: undefined, key4: null}} | ${'key1=123&key2=1&key4=null'}
-    ${{key1: '', key2: 0, key3: new Date("2024-06-09T12:34:56.789Z")}} | ${'key1=&key2=0&key3=%222024-06-09T12%3A34%3A56.789Z%22'}
-    ${{key1: '', key2: 0, key3: new Date()}} | ${`key1=&key2=0&key3=${encodeURIComponent(JSON.stringify(new Date()))}`}
-    ${{key1: {value: '123'}}} | ${'key1=%7B%22value%22%3A%22123%22%7D'}
+    params                                                                         | output
+    ${{ key1: true, key2: false }}                                                 | ${"key1=true&key2=false"}
+    ${{ key1: "123", key2: 1, key3: undefined, key4: null }}                       | ${"key1=123&key2=1&key4=null"}
+    ${{ key1: "", key2: 0, key3: new Date("2024-06-09T12:34:56.789Z") }}           | ${"key1=&key2=0&key3=%222024-06-09T12%3A34%3A56.789Z%22"}
+    ${{ key1: "", key2: 0, key3: new Date() }}                                     | ${`key1=&key2=0&key3=${encodeURIComponent(JSON.stringify(new Date()))}`}
+    ${{ key1: { value: "123" } }}                                                  | ${"key1=%7B%22value%22%3A%22123%22%7D"}
+    ${{ key1: ["123", null, undefined, 1, new Date("2024-06-09T12:34:56.789Z")] }} | ${"key1=%5B%22123%22%2Cnull%2Cnull%2C1%2C%222024-06-09T12%3A34%3A56.789Z%22%5D"}
   `("test case serializeParams %#", ({ params, output }) => {
     expect(fallbackOptions.serializeParams(params)).toBe(output);
   });
