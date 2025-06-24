c6f67c6436eba3c9e40195b58e0e9f06440b0421
momotom <a18520145493@163.com>
2025-06-24 22:33:45 +0800

    test: Key is a 2-dimensional array.

--- test/fallback-options.spec.ts
+++ test/fallback-options.spec.ts
@@ -12,6 +12,7 @@ describe("serializeParams", () => {
     ${{ key1: { value: "123" } }}                                                  | ${"key1=%7B%22value%22%3A%22123%22%7D"}
     ${{ key1: ["123", null, undefined, 1, new Date("2024-06-09T12:34:56.789Z")] }} | ${"key1=%5B%22123%22%2Cnull%2Cnull%2C1%2C%222024-06-09T12%3A34%3A56.789Z%22%5D"}
     ${{ key1: [[1, "123", null, false, undefined]] }}                              | ${"key1=%5B%5B1%2C%22123%22%2Cnull%2Cfalse%2Cnull%5D%5D"}
+    ${{ key1: [1, [2, false, null]] }}                                             | ${"key1=%5B1%2C%5B2%2Cfalse%2Cnull%5D%5D"}
   `("test case serializeParams %#", ({ params, output }) => {
     expect(fallbackOptions.serializeParams(params)).toBe(output);
   });
