0a51454cef94b262374cceddf678d106b6a90bfb
momotom <a18520145493@163.com>
2025-06-29 22:58:41 +0800

    test: Basic object consolidation => Should handle undefined and null values

--- test/merge-options.spec.ts
+++ test/merge-options.spec.ts
@@ -19,5 +19,20 @@ describe("mergeOptions", () => {
         shared: "from-opt4",
       });
     });
+
+    test("Should handle undefined and null values", () => {
+      const opt1 = { a: 1, b: "keep", c: "will-be-null" };
+      const opt2 = { a: undefined, b: null, c: null };
+      const opt3 = { d: "new" };
+      const opt4 = {};
+
+      const result = mergeOptions(opt1, opt2, opt3, opt4);
+      expect(result).toEqual({
+        a: undefined,
+        b: null,
+        c: null,
+        d: "new",
+      });
+    });
   });
 });
